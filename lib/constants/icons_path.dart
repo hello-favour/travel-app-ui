c11cba78a26355b082bcb98b6ca2e44a9498c357
Favour <hello.williamsfav@gmail.com>
2023-05-10 02:45:05 +0100

    set up project

---
+++ lib/constants/icons_path.dart
@@ -0,0 +1,15 @@
+class IconsPath {
+  static const String calender = "assets/icons/calender.png";
+  static const String clock = "assets/icons/clock.png";
+  static const String comments = "assets/icons/comments.png";
+  static const String condo = "assets/icons/condo.png";
+  static const String flight = "assets/icons/flight.png";
+  static const String frontBus = "assets/icons/front_bus.png";
+  static const String home = "assets/icons/home.png";
+  static const String home2 = "assets/icons/home2.png";
+  static const String plate = "assets/icons/plate.png";
+  static const String rating = "assets/icons/rating.png";
+  static const String saveInstagram = "assets/icons/save_instagram.png";
+  static const String search = "assets/icons/search.png";
+  static const String send = "assets/icons/send.png";
+}
---
+++ lib/constants/image_path.dart
@@ -0,0 +1,7 @@
+class ImagePath {
+  static const String aroplane = "assets/images/aroplane.png";
+  static const String bg = "assets/images/bg.png";
+  static const String girlTravel = "assets/images/girl_travel.png";
+  static const String girl = "assets/images/girl.png";
+  static const String world = "assets/images/world.png";
+}
