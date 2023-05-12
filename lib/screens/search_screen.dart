00ca622e51609c306d7cefeeb7d6c00159325c01
Favour <hello.williamsfav@gmail.com>
2023-05-12 15:30:50 +0100

    build search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:travel_app_ui/themes/app_colors.dart';
+import 'package:travel_app_ui/themes/card_padding.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,10 +11,47 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  var height, width;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Container(
+          height: height * 0.073,
+          width: width * 0.9,
+          decoration: BoxDecoration(
+            color: Colors.blue.withOpacity(0.3),
+            borderRadius: CardPadding.appRadius,
+          ),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(left: width * 0.03),
+              child: TextFormField(
+                style: const TextStyle(
+                  color: AppColors.appWhite,
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search here...",
+                  hintStyle: GoogleFonts.lexend(
+                    color: Colors.grey,
+                    fontSize: width * 0.04,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black54,
+                    size: width * 0.09,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
